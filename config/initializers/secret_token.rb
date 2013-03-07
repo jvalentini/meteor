ade94b9c133df133b93d54758d135d8e83e27113
Justin Valentini <justin.valentini@gmail.com>
2013-03-06 21:53:22 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meteor::Application.config.secret_token = 'b9e3b2386c4f49a2bdec8a6143b87080be65dd4eb1851be156ba2a606d12c32e2ad151946368d643353e11687cad14a662163a7dcec4f060f0b201bdf0e04941'
